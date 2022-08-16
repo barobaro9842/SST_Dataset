627e178f9f479bfee13ffa63563b0bf7714128f2
barobaro9842 <baro9842@gmail.com>
2022-08-16 11:58:59 +0900

    download data update - overlap check, recent 6days

--- test/netCDFfunc/utility.py
+++ test/netCDFfunc/utility.py
@@ -494,84 +494,84 @@ def download_data(output_path, start_date=None, end_date=None, dataset_names=Non
     if dataset_names == None :
         dataset_names = ['AVHRR', 'CMC', 'DMI', 'GAMSSA', 'MUR25', 'MUR', 'MWIR', 'MW', 'NAVO', 'OSPON', 'OSPO', 'OSTIA']
 
-    if start_date == None and end_date == None :
+    if start_date == None and end_date == None : date_range = range(6,0,-1)
         
-        for delta in range(6,0,-1):
-            
-            recent = datetime.datetime.now()-datetime.timedelta(days=delta)
-            year = recent.strftime('%Y')
-            date = recent.strftime('%Y%m%d')
-            
-            j_day = recent.strftime('%j')
-            j_day = '%03d' % int(j_day)
+    for delta in date_range:
+        
+        recent = datetime.datetime.now()-datetime.timedelta(days=delta)
+        year = recent.strftime('%Y')
+        date = recent.strftime('%Y%m%d')
+        
+        j_day = recent.strftime('%j')
+        j_day = '%03d' % int(j_day)
 
-            for dataset_name in dataset_names :
+        for dataset_name in dataset_names :
+            
+            if dataset_name == 'AVHRR':
+                file_name = f'{date}120000-NCEI-L4_GHRSST-SSTblend-AVHRR_OI-GLOB-v02.0-fv02.1.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/NCEI/AVHRR_OI/v2.1/{year}/{j_day}/{file_name}'
+            
+            elif dataset_name == 'CMC' :
+                file_name = f'{date}120000-CMC-L4_GHRSST-SSTfnd-CMC0.1deg-GLOB-v02.0-fv03.0.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/CMC/CMC0.1deg/v3/{year}/{j_day}/{file_name}'
                 
-                if dataset_name == 'AVHRR':
-                    file_name = f'{date}120000-NCEI-L4_GHRSST-SSTblend-AVHRR_OI-GLOB-v02.0-fv02.1.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/NCEI/AVHRR_OI/v2.1/{year}/{j_day}/{file_name}'
+            elif dataset_name == 'DMI' :
+                file_name = f'{date}000000-DMI-L4_GHRSST-SSTfnd-DMI_OI-GLOB-v02.0-fv01.0.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/DMI/DMI_OI/v1/{year}/{j_day}/{file_name}'
                 
-                elif dataset_name == 'CMC' :
-                    file_name = f'{date}120000-CMC-L4_GHRSST-SSTfnd-CMC0.1deg-GLOB-v02.0-fv03.0.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/CMC/CMC0.1deg/v3/{year}/{j_day}/{file_name}'
-                    
-                elif dataset_name == 'DMI' :
-                    file_name = f'{date}000000-DMI-L4_GHRSST-SSTfnd-DMI_OI-GLOB-v02.0-fv01.0.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/DMI/DMI_OI/v1/{year}/{j_day}/{file_name}'
-                    
-                elif dataset_name == 'GAMSSA' :
-                    file_name = f'{date}120000-ABOM-L4_GHRSST-SSTfnd-GAMSSA_28km-GLOB-v02.0-fv01.0.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/ABOM/GAMSSA/v1.0/{year}/{j_day}/{file_name}'
-                    
-                elif dataset_name == 'MUR25' :
-                    file_name = f'{date}090000-JPL-L4_GHRSST-SSTfnd-MUR25-GLOB-v02.0-fv04.2.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/JPL/MUR25/v4.2/{year}/{j_day}/{file_name}'
-                    
-                elif dataset_name == 'MUR' :
-                    file_name = f'{date}090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/JPL/MUR/v4.1/{year}/{j_day}/{file_name}'
-                    
-                elif dataset_name == 'MWIR' :
-                    file_name = f'{date}120000-REMSS-L4_GHRSST-SSTfnd-MW_IR_OI-GLOB-v02.0-fv05.0.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/REMSS/mw_ir_OI/v5.0/{year}/{j_day}/{file_name}'
-                    
-                elif dataset_name == 'MW' :
-                    file_name = f'{date}120000-REMSS-L4_GHRSST-SSTfnd-MW_OI-GLOB-v02.0-fv05.0.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/REMSS/mw_OI/v5.0/{year}/{j_day}/{file_name}'
-                    
-                elif dataset_name == 'NAVO' :
-                    file_name = f'{date}000000-NAVO-L4_GHRSST-SST1m-K10_SST-GLOB-v02.0-fv01.0.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/NAVO/K10_SST/v1/{year}/{j_day}/{file_name}'
-                    
-                elif dataset_name == 'OSPON' :
-                    file_name = f'{date}000000-OSPO-L4_GHRSST-SSTfnd-Geo_Polar_Blended_Night-GLOB-v02.0-fv01.0.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/OSPO/Geo_Polar_Blended_Night/v1/{year}/{j_day}/{file_name}'
-                    
-                elif dataset_name == 'OSPO' :
-                    file_name = f'{date}000000-OSPO-L4_GHRSST-SSTfnd-Geo_Polar_Blended-GLOB-v02.0-fv01.0.nc'
-                    url = f'{common_url}/GDS2/L4/GLOB/OSPO/Geo_Polar_Blended/v1/{year}/{j_day}/{file_name}'
-                    
-                elif dataset_name == 'OSTIA' :
-                    file_name = f'{date}-UKMO-L4HRfnd-GLOB-v01-fv02-OSTIA.nc.bz2'
-                    url = f'{common_url}/L4/GLOB/UKMO/OSTIA/{year}/{j_day}/{file_name}'
+            elif dataset_name == 'GAMSSA' :
+                file_name = f'{date}120000-ABOM-L4_GHRSST-SSTfnd-GAMSSA_28km-GLOB-v02.0-fv01.0.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/ABOM/GAMSSA/v1.0/{year}/{j_day}/{file_name}'
+                
+            elif dataset_name == 'MUR25' :
+                file_name = f'{date}090000-JPL-L4_GHRSST-SSTfnd-MUR25-GLOB-v02.0-fv04.2.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/JPL/MUR25/v4.2/{year}/{j_day}/{file_name}'
+                
+            elif dataset_name == 'MUR' :
+                file_name = f'{date}090000-JPL-L4_GHRSST-SSTfnd-MUR-GLOB-v02.0-fv04.1.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/JPL/MUR/v4.1/{year}/{j_day}/{file_name}'
+                
+            elif dataset_name == 'MWIR' :
+                file_name = f'{date}120000-REMSS-L4_GHRSST-SSTfnd-MW_IR_OI-GLOB-v02.0-fv05.0.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/REMSS/mw_ir_OI/v5.0/{year}/{j_day}/{file_name}'
+                
+            elif dataset_name == 'MW' :
+                file_name = f'{date}120000-REMSS-L4_GHRSST-SSTfnd-MW_OI-GLOB-v02.0-fv05.0.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/REMSS/mw_OI/v5.0/{year}/{j_day}/{file_name}'
+                
+            elif dataset_name == 'NAVO' :
+                file_name = f'{date}000000-NAVO-L4_GHRSST-SST1m-K10_SST-GLOB-v02.0-fv01.0.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/NAVO/K10_SST/v1/{year}/{j_day}/{file_name}'
+                
+            elif dataset_name == 'OSPON' :
+                file_name = f'{date}000000-OSPO-L4_GHRSST-SSTfnd-Geo_Polar_Blended_Night-GLOB-v02.0-fv01.0.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/OSPO/Geo_Polar_Blended_Night/v1/{year}/{j_day}/{file_name}'
+                
+            elif dataset_name == 'OSPO' :
+                file_name = f'{date}000000-OSPO-L4_GHRSST-SSTfnd-Geo_Polar_Blended-GLOB-v02.0-fv01.0.nc'
+                url = f'{common_url}/GDS2/L4/GLOB/OSPO/Geo_Polar_Blended/v1/{year}/{j_day}/{file_name}'
+                
+            elif dataset_name == 'OSTIA' :
+                file_name = f'{date}-UKMO-L4HRfnd-GLOB-v01-fv02-OSTIA.nc.bz2'
+                url = f'{common_url}/L4/GLOB/UKMO/OSTIA/{year}/{j_day}/{file_name}'
 
-                if file_name.endswith('.bz2') : file_name = file_name.replace('.bz2', '')
-                target_file = os.path.join(output_path, dataset_name, file_name)
+            if file_name.endswith('.bz2') : file_name = file_name.replace('.bz2', '')
+            target_file = os.path.join(output_path, dataset_name, file_name)
+            
+            if os.path.exists(target_file) and os.path.getsize(target_file) > 5 * 1024:
+                print(f'[{date}|{dataset_name}] is already downloaded!')
+                continue
+            
+            response = requests.get(url, stream=True)
+            
+            if response.status_code == 404:
+                print(f'[{date}|{dataset_name}] doesn\'t exist!')
+                continue
+            
+            if not os.path.exists(os.path.join(output_path, dataset_name)) : os.mkdir(os.path.join(output_path, dataset_name))
+            
+            with open(target_file, 'wb') as out_file:
+                print(f'[{date}|{dataset_name}] downloading...')
+                shutil.copyfileobj(response.raw, out_file)
                 
-                if os.path.exists(target_file) and os.path.getsize(target_file) > 5 * 1024:
-                    print(f'[{date}|{dataset_name}] is already downloaded!')
-                    continue
-                
-                response = requests.get(url, stream=True)
-                
-                if response.status_code == 404:
-                    print(f'[{date}|{dataset_name}] doesn\'t exist!')
-                    continue
-                
-                if not os.path.exists(os.path.join(output_path, dataset_name)) : os.mkdir(os.path.join(output_path, dataset_name))
-                
-                with open(target_file, 'wb') as out_file:
-                    print(f'[{date}|{dataset_name}] downloading...')
-                    shutil.copyfileobj(response.raw, out_file)
-                    
-                del response
+            del response
